91c96be7a796934fcaca5980d17e6b309a47c18e
shashlji <shashank.singh@lji.io>
2024-03-06 14:56:38 +0530

    update

--- build/lib/buddy/cli.py
+++ build/lib/buddy/cli.py
@@ -2,9 +2,10 @@ import click
 import os
 from .comet import comet
 from .cw import cw
+from .creds import creds
 
 @click.command()
-@click.option("--t", "--type" , default = None, help="possible values are - comet, cw/cloudwatch, tf")
+@click.option("--t", "--type" , default = None, help="possible values are - comet, cw/cloudwatch, tf, creds")
 @click.option("--e", '--env', default=None, help="env name")
 @click.option("--p", '--profile', default=None, help="aws creentials profile")
 @click.option("--m", '--mode', default=None, help="comet mode")
@@ -12,8 +13,11 @@ from .cw import cw
 @click.option("--s",'--snippet', default=None, help="name of snippet")
 @click.option("--d",'--delete', default=None, help="name of snippet file to be deleted (with extention)")
 def cli(t,e,p,m,c,s,d):
-    click.echo('Welcome to buddy')
     if t == "comet":
         comet.run(m , e , p)
     elif t == "cw" or t == "cloudwatch":
         cw.run(e , p)
+    elif t == "creds":
+        creds.run()
+    else:
+        click.echo('Welcome to buddy')
